27c3255f8d822a0efcaec1d0e868fa483a15c171
AhmedMaiza <92824053+AhmedMaiza@users.noreply.github.com>
2021-12-08 20:38:29 +0100

    atelier 8

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,42 @@
+import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { User } from '../model/user.model';
+import { AuthService } from '../service/auth.service';
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styles: [
+  ]
+})
+export class LoginComponent implements OnInit {
+  user = new User();
+  erreur =0
+
+  constructor(private authService : AuthService,
+    private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+
+ onLoggedin(){
+/*console.log(this.user);
+let isValidUser: Boolean = this.authService.SignIn(this.user);
+if (isValidUser)
+this.router.navigate(['/']);
+else
+this.erreur=1;*/
+this.authService.getUserFromDB(this.user.username).subscribe((usr:User) => {
+  if (usr.password==this.user.password)
+  {
+    this.authService.signIn(usr);
+    this.router.navigate(['/']);
+    }
+    else
+    this.erreur = 1;
+    },(err) => console.log(err));
+}
+
+}
